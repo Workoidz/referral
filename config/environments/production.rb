7212576c599b4fc7422d965708bf4b5c812a8c26
Ritesh Kapoor <ritesh@workoidz.com>
2023-06-06 18:47:38 +0530

    Gmail settings removed for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,16 +26,13 @@ Rails.application.configure do
 
   
   # for devise
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  user_name:            'ritesh@9techies.com',
-  password:             '=F8*jPaY1',
-  authentication:       'plain',
-  enable_starttls_auto: true
-}
-  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
